4fbe15966af7bf1c2762598f9559fcaa97cccadb
Steve.Aristotle <way0utwest@hotmail.com>
2015-08-25 14:47:22 -0600

    Sample data for teams

---
+++ Data/Teams_Data.sql
@@ -0,0 +1,11 @@
+INSERT dbo.Teams
+        ( teamcode, teamname )
+VALUES  ( 'NYY', 'New York Yankees')
+     ,  ( 'BOS', 'Boston Red Sox')
+     ,  ( 'DAL', 'Dallas Cowboys')
+     ,  ( 'DAL', 'Dallas Mavericks')
+     ,  ( 'NYG', 'New York Giants')
+     ,  ( 'PHI', 'Philadelphia Eagles')
+     ,  ( 'DEN', 'Denver Broncos')
+     ,  ( 'PHI', 'Philadelphia 76ers')
+        
