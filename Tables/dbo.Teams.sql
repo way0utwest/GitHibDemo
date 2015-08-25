ea257fc557c16f93ffc6c760352b2ada4b9e05c4
Steve.Aristotle <way0utwest@hotmail.com>
2015-08-25 14:47:04 -0600

    Added FK from Teams to Sports to ensure teams are classified by sport.

--- Tables/dbo.Teams.sql
+++ Tables/dbo.Teams.sql
@@ -1,7 +1,12 @@
 CREATE TABLE [dbo].[Teams]
 (
-[teamid] [int] NULL,
+[teamid] [int] NOT NULL,
 [teamcode] [varchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[teamname] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[teamname] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[SportID] [int] NULL
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[Teams] ADD 
+CONSTRAINT [PK_Teams] PRIMARY KEY CLUSTERED  ([teamid]) ON [PRIMARY]
+ALTER TABLE [dbo].[Teams] ADD
+CONSTRAINT [FK_Teams_sports] FOREIGN KEY ([SportID]) REFERENCES [dbo].[sports] ([sportid])
 GO
