e30d635f3f7fc14971d90dbb6bbb13eb84f2bd73
Steve.Aristotle <way0utwest@hotmail.com>
2015-08-25 15:02:51 -0600

    Sample Data for predictions
    3 game predictions for baseball from two users.

---
+++ Data/gamepredictions_data.sql
@@ -0,0 +1,28 @@
+INSERT dbo.GamePredictions
+        ( Userid, scheduleid, winner )
+VALUES  ( 1  -- Userid - int
+          , 1  -- scheduleid - int
+          , 1  -- winner - int
+          )
+		, ( 1  -- Userid - int
+          , 2  -- scheduleid - int
+          , 2 -- winner - int
+          )
+		, ( 1  -- Userid - int
+          , 3  -- scheduleid - int
+          , 1 -- winner - int
+          )
+		, ( 2  -- Userid - int
+          , 1  -- scheduleid - int
+          , 2 -- winner - int
+          )
+		, ( 2  -- Userid - int
+          , 2  -- scheduleid - int
+          , 2 -- winner - int
+          )
+		, ( 2  -- Userid - int
+          , 3  -- scheduleid - int
+          , 2 -- winner - int
+          )
+
+
