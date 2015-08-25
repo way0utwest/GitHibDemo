ce6f6bdbcf9ae48879be9e29f6508edbc5c6b6da
Steve.Aristotle <way0utwest@hotmail.com>
2015-08-25 15:01:35 -0600

    Added a column to contain the winning FK for the game.

--- Tables/dbo.Schedules.sql
+++ Tables/dbo.Schedules.sql
@@ -3,7 +3,8 @@ CREATE TABLE [dbo].[Schedules]
 [scheduleid] [int] NOT NULL IDENTITY(1, 1),
 [visitingteam] [int] NULL,
 [hometeam] [int] NULL,
-[gamedate] [datetime] NULL
+[gamedate] [datetime] NULL,
+[winner] [int] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Schedules] ADD CONSTRAINT [PK__Schedule__A533E9ED35FC20A2] PRIMARY KEY NONCLUSTERED  ([scheduleid]) ON [PRIMARY]
